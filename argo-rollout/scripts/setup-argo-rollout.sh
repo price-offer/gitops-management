cee9246c482cb440a868765e5c2fd56aeeb09210
지훈 <goharrm@gmail.com>
2023-01-22 16:57:22 +0900

    fix: change darwin to linux

--- argo-rollout/scripts/setup-argo-rollout.sh
+++ argo-rollout/scripts/setup-argo-rollout.sh
@@ -5,9 +5,9 @@ kubectl create namespace argo-rollouts
 kubectl apply -n argo-rollouts -f https://github.com/argoproj/argo-rollouts/releases/latest/download/install.yaml
 
 # 3. Kubectl Plugin 설치(linux)
-curl -LO https://github.com/argoproj/argo-rollouts/releases/latest/download/kubectl-argo-rollouts-darwin-amd64
-chmod +x ./kubectl-argo-rollouts-darwin-amd64
-sudo mv ./kubectl-argo-rollouts-darwin-amd64 /usr/local/bin/kubectl-argo-rollouts
+curl -LO https://github.com/argoproj/argo-rollouts/releases/latest/download/kubectl-argo-rollouts-linux-amd64
+chmod +x ./kubectl-argo-rollouts-linux-amd64
+sudo mv ./kubectl-argo-rollouts-linux-amd64 /usr/local/bin/kubectl-argo-rollouts
 
 # 4. Argo Rollout Dashboard 기동 (http://localhost:3100)
 kubectl argo rollouts dashboard
