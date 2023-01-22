a0a49840c913bf8318878a0a2b5d7c8bef42f54d
지훈 <goharrm@gmail.com>
2023-01-22 16:53:09 +0900

    edit argo rollout install script

--- argo-rollout/scripts/setup-argo-rollout.sh
+++ argo-rollout/scripts/setup-argo-rollout.sh
@@ -2,9 +2,9 @@
 kubectl create namespace argo-rollouts
 
 # 2. Argo Rollout 설치
-kubectl kustomize ../manifests/ | kubectl apply -f -
+kubectl apply -n argo-rollouts -f https://github.com/argoproj/argo-rollouts/releases/latest/download/install.yaml
 
-# 3. Argo Rollout 플러그인 설치(linux)
+# 3. Kubectl Plugin 설치(linux)
 curl -LO https://github.com/argoproj/argo-rollouts/releases/latest/download/kubectl-argo-rollouts-darwin-amd64
 chmod +x ./kubectl-argo-rollouts-darwin-amd64
 sudo mv ./kubectl-argo-rollouts-darwin-amd64 /usr/local/bin/kubectl-argo-rollouts
